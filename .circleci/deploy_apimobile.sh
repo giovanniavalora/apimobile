d5bba6616658f290be487e2cbc109debb71acd97
giova <giovanni.aravena@avalora.com>
2020-03-17 23:42:35 -0300

    gitflow-hotfix-stash: migrations

--- .circleci/deploy_apimobile.sh
+++ .circleci/deploy_apimobile.sh
@@ -3,6 +3,6 @@ gitcheckout develop
 git pull
 cd /home/ohl/controlCamionesApi/
 docker-compose -f docker-compose.prod.yml up -d --build
-#docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput
-#docker-compose -f docker-compose.prod.yml exec web python manage.py makemigrations --noinput
-#docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
+docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput
+docker-compose -f docker-compose.prod.yml exec web python manage.py makemigrations --noinput
+docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
