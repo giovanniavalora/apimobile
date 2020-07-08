81d5a6499c5ac16052d1d5563ec9a5dd6c620029
giova <giovanni.aravena@avalora.com>
2020-07-08 18:14:23 -0400

    id_qr a charfield

--- api/models.py
+++ api/models.py
@@ -298,7 +298,7 @@ class Voucher(models.Model):
     punto_suborigen = models.CharField(max_length = 100, blank=True)
     punto_destino = models.CharField(max_length = 100)
     contador_impresiones = models.IntegerField()
-    id_qr = models.CharField(blank=True)
+    id_qr = models.CharField(max_length = 255, blank=True)
     def __str__(self):
         cadena = "voucher_"+str(self.id)+" "+str(self.despachador)
         return cadena
